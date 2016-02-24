55584b1f118955c24a93b5ed71a9e19451b9d306
Ryan Rector <rmrector@gmail.com>
2016-02-24 05:00:01 -0700

    Other JSON cleanup, fix Python 2.6 issue

--- resources/lib/quickjson.py
+++ resources/lib/quickjson.py
@@ -51,31 +51,18 @@ def get_base_json_request(method):
 
 def execute_jsonrpc(jsonrpc_command):
     if isinstance(jsonrpc_command, dict):
-        jsonrpc_command = json.dumps(jsonrpc_command, ensure_ascii=False)
-        if isinstance(jsonrpc_command, unicode):
-            jsonrpc_command = jsonrpc_command.encode('utf-8')
+        jsonrpc_command = json.dumps(jsonrpc_command)
 
     json_result = xbmc.executeJSONRPC(jsonrpc_command)
-    return _json_unicode_to_str(json.loads(json_result))
-
-def _json_unicode_to_str(jsoninput):
-    """Converts values in a data object from JSON to utf-8."""
-    if isinstance(jsoninput, dict):
-        return {key: _json_unicode_to_str(value) for key, value in jsoninput.iteritems()}
-    elif isinstance(jsoninput, list):
-        return [_json_unicode_to_str(item) for item in jsoninput]
-    elif isinstance(jsoninput, unicode):
-        return jsoninput.encode('utf-8')
-    else:
-        return jsoninput
+    return json.loads(json_result, cls=UTF8JSONDecoder)
 
 def _check_json_result(json_result, result_key, json_request):
     if 'error' in json_result:
-        raise JsonException(json_request, json_result)
+        raise JSONException(json_request, json_result)
 
     return 'result' in json_result and result_key in json_result['result']
 
-class JsonException(Exception):
+class JSONException(Exception):
     def __init__(self, json_request, json_result):
         self.json_request = json_request
         self.json_result = json_result
@@ -87,7 +74,7 @@ class JsonException(Exception):
 
         if isinstance(message, unicode):
             message = message.encode('utf-8')
-        super(JsonException, self).__init__(message)
+        super(JSONException, self).__init__(message)
 
 class LogJSONEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -98,3 +85,22 @@ class LogJSONEncoder(json.JSONEncoder):
         if hasattr(obj, '__dict__'):
             return obj.__dict__
         return str(obj)
+
+class UTF8JSONDecoder(json.JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        super(UTF8JSONDecoder, self).__init__(*args, **kwargs)
+
+    def raw_decode(self, s, idx=0):
+        result, end = super(UTF8JSONDecoder, self).raw_decode(s)
+        result = self._json_unicode_to_str(result)
+        return result, end
+
+    def _json_unicode_to_str(self, jsoninput):
+        if isinstance(jsoninput, dict):
+            return dict((self._json_unicode_to_str(key), self._json_unicode_to_str(value)) for key, value in jsoninput.iteritems())
+        elif isinstance(jsoninput, list):
+            return [self._json_unicode_to_str(item) for item in jsoninput]
+        elif isinstance(jsoninput, unicode):
+            return jsoninput.encode('utf-8')
+        else:
+            return jsoninput
